5130b31930b042bd849965f38b26024376f37707
Evgenii Khramkov <ewgeniux@gmail.com>
2020-07-24 07:37:30 -0700

    update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,21 @@
 import "react-app-polyfill/ie11";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Thing } from "../.";
+import "./index.css";
+import { Button, ThemeContext, defaultTheme } from "../src";
+
+const theme = {
+  ...defaultTheme,
+};
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div className="container mx-auto">
+        <h1 className="text-lg">Example</h1>
+        <Button>test</Button>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
